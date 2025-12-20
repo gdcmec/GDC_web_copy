188122aa25f23689f3899fa3ef568d917e54a296
Hashim <hashimmohammed212@gmail.com>
2025-12-20 10:46:40 +0530

    Refactor Landing component: Replace background image with SVG and enhance styling for improved visual presentation

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -17,15 +17,17 @@ const landing = () => {
   return (
     <>
 
-      <div
-        className="relative flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-black"
-        style={{
-          backgroundImage: `url(${unionBg})`,
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-          backgroundRepeat: 'no-repeat'
-        }}
-      >
+      <div className="relative flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-black">
+        <svg className='absolute top-8 bottom-8 left-4 right-8 w-[calc(100%-3rem)] h-[calc(100%-4rem)]' viewBox="0 0 1887 1018" fill="none" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg">
+          <path fillRule="evenodd" clipRule="evenodd" d="M1341.48 69.3856H536.642C518.971 69.3856 502.122 61.9232 490.246 48.8375C488.355 46.7538 486.436 44.6005 484.487 42.4122C466.867 22.637 446.697 0 421.27 0H65C29.1015 0 0 29.1015 0 65V891.899C0 927.798 29.1015 956.899 65.0001 956.899H559.666L611.275 1007.9C617.826 1014.37 626.665 1018 635.875 1018H1249.36C1258.43 1018 1267.15 1014.48 1273.67 1008.18L1326.8 956.899H1822C1857.9 956.899 1887 927.798 1887 891.899V65C1887 29.1015 1857.9 0 1822 0H1459.94C1434.65 0 1414.18 21.4471 1395.66 40.8402C1392.3 44.3623 1389 47.8167 1385.75 51.0626C1374.01 62.7954 1358.08 69.3856 1341.48 69.3856Z" fill="url(#paint0_linear_252_21)" />
+          <defs>
+            <linearGradient id="paint0_linear_252_21" x1="943.5" y1="0" x2="943.5" y2="1018" gradientUnits="userSpaceOnUse">
+              <stop offset="0.315674" stopColor="#3D3D3D" />
+              <stop offset="1" stopColor="#1F1F1F" />
+            </linearGradient>
+          </defs>
+        </svg>
+
 
         <img className="hidden z-10" src={logo} alt="" />
         <motion.img
